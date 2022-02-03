e285ee39bee9554560a983b5c5811d1f7bae5b73
umutinceer <mumutinceer@gmail.com>
2022-02-03 20:01:13 +0300

    header is now visible at initial page load

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,3 +1,73 @@
 import createHomePage from './homepage.js';
+import './style.css';
 
-createHomePage();
+window.onload = function(){
+    loadCleanPage.execute();
+    createHomePage();
+}
+
+const divContent = document.getElementById('content');
+
+
+const changePage = (function(){
+
+    function loadHomePage(){
+        loadCleanPage.execute();
+        createHomePage();
+    }
+
+    function loadMenuPage(){
+        console.log("menupage!");
+    }
+
+    function loadContactUsPage(){
+        console.log("contactus!");
+    }
+
+    return [loadHomePage,loadMenuPage,loadContactUsPage];
+})();
+
+
+const loadCleanPage = (function(){
+    function createHeader(){
+        const divHeader = document.createElement('div');
+        divHeader.setAttribute('id','header');
+    
+        const h1 = document.createElement('h1');
+        h1.innerText = 'Sonradan Gourmet';
+    
+        const nav = document.createElement('nav');
+        nav.setAttribute('id','nav');
+    
+        const ul = document.createElement('ul');
+    
+        const navbarArray = ['Home','Menu','Contact Us'];
+    
+        for (let i=0; i<3; i++) {
+            let li = document.createElement('li');
+            li.innerText = navbarArray[i];
+            li.addEventListener('click',changePage[i]);
+            ul.appendChild(li);
+        }
+    
+        nav.appendChild(h1);
+        nav.appendChild(ul);
+    
+        divHeader.appendChild(nav);
+        divContent.appendChild(divHeader);
+    }
+    
+    function clearPage(){
+        divContent.innerHTML = '';
+    }
+
+    function execute(){
+        clearPage();
+        createHeader();
+    }
+
+    return{
+        execute
+    }
+})();
+
